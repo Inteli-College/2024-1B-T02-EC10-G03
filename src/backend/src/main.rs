2ab2ac6029c8f430acbeb2f0733544f91406d053
Pablo Ruan Lana Viana <pablo.viana@sou.inteli.edu.br>
2024-05-08 13:53:59 -0300

    feat(backend): :sparkles: Create routes to medicines catalog

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -47,6 +47,11 @@ struct PyxisInput {
 	block: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct MedicineInput {
+	name: String,
+}
+
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct User {
 	id: i32,
@@ -91,6 +96,26 @@ async fn delete_pyxis(state: web::types::State<Arc<AppState>>, uuid: web::types:
 	let pyxis = state.db.pyxis().delete(pyxis::uuid::equals(uuid.into_inner())).exec().await.unwrap();
 	HttpResponse::Ok().json(&pyxis)
 }
+#[web::get("/catalog")]
+async fn get_all_catalog(state: web::types::State<Arc<AppState>>) -> HttpResponse {
+	let medicine = state.db.medicine_name().find_many(vec![]).exec().await.unwrap();
+	HttpResponse::Ok().json(&medicine)
+}
+#[web::get("/catalog/{uuid}")]
+async fn get_catalog(state: web::types::State<Arc<AppState>>, uuid: web::types::Path<String>) -> HttpResponse {
+	let medicine = state.db.medicine_name().find_unique(medicine_name::uuid::equals(uuid.into_inner())).exec().await.unwrap();
+	HttpResponse::Ok().json(&medicine)
+}
+#[web::post("/catalog")]
+async fn create_catalog(state: web::types::State<Arc<AppState>>, medicine: web::types::Json<MedicineInput>) -> HttpResponse {
+	let medicine = state.db.medicine_name().create(medicine.name.to_string(), vec![]).exec().await.unwrap();
+	HttpResponse::Created().json(&medicine)
+}
+#[web::delete("/catalog/{uuid}")]
+async fn delete_catalog(state: web::types::State<Arc<AppState>>, uuid: web::types::Path<String>) -> HttpResponse {
+	let medicine = state.db.medicine_name().delete(medicine_name::uuid::equals(uuid.into_inner())).exec().await.unwrap();
+	HttpResponse::Ok().json(&medicine)
+}
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
 	dotenvy::dotenv().ok();
@@ -132,10 +157,16 @@ async fn main() -> std::io::Result<()> {
 			.configure(routes::inventory::inventory_config)
 			.configure(routes::user::user_config)
 			.service(index)
+			
 			.service(get_all_pyxis)
+			.service(get_pyxis)
 			.service(create_pyxis)
 			.service(delete_pyxis)
-			.service(get_pyxis)
+
+			.service(get_all_catalog)
+			.service(get_catalog)
+			.service(create_catalog)
+			.service(delete_catalog)
 	})
 	.bind("0.0.0.0:3000")?
 	.run()
