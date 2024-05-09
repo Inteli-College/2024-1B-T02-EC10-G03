0bfca5c83b37aa002ead2840de75555cb77f1165
HallZero <filipikikuchi@gmail.com>
2024-05-09 10:20:48 -0300

    test(load_test): :stethoscope: Update load_test for new endpoints

--- src/load_test/api_load_test.ts
+++ src/load_test/api_load_test.ts
@@ -1,12 +1,14 @@
 import http from 'k6/http';
+import {sleep} from 'k6';
+import { randomIntBetween, uuidv4 } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
 
     blacklistIPs: [],
 
     thresholds: {
-        http_req_failed: ['rate<0.01'], 
-        http_req_duration: ['p(95)<200'],
+        http_req_failed: ['rate<0.05'], 
+        http_req_duration: ['p(95)<300'],
     },
 
     vus: 10,
@@ -14,11 +16,31 @@ export const options = {
 };
 
 export default function () {
-    const url = 'http://localhost:3000/';
+    const baseUrl = 'http://localhost:3000';
     
-    http.get(url);
+    http.get(baseUrl);
 
-    http.get(url + 'swagger.json');
+    http.get(baseUrl + '/pyxis');
+    http.get(baseUrl + '/catalog');
+
+    // Creating a new pyxis entry
+    const pyxisPayload = JSON.stringify({ floor: randomIntBetween(1, 11), block: 'exampleBlock' });
+    http.post(baseUrl + '/pyxis', pyxisPayload, { headers: { 'Content-Type': 'application/json' } });
+
+    // Creating a new medicine
+    const catalogPayload = JSON.stringify({ name: 'exampleMedicineName' });
+    http.post(baseUrl + '/catalog', catalogPayload, { headers: { 'Content-Type': 'application/json' } });
+
+    // Deleting a pyxis entry and medicine
+    // Currently, the delete operation is leading
+    // to a error because of the uuid not existing
+    
+    /*
+    http.del(baseUrl + '/pyxis/' + uuidv4());
+    http.del(baseUrl + '/catalog/' + uuidv4());
+    */
+
+    sleep(1);
 
 };
 
