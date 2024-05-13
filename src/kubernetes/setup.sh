6ab8511466139e2df2805b7f76498886950db819
cmtabr <caio.abreu@sou.inteli.edu.br>
2024-05-12 21:07:09 -0300

    feat(shell-miikube-setup): :sparkles: Added basic shell to initilize kubernetes orchestrator

---
+++ src/kubernetes/setup.sh
@@ -0,0 +1,64 @@
+echo "\n---------------------------------"
+echo "| Kubernetes deployment started |"
+echo "---------------------------------\n"
+
+sleep 1
+
+echo "    GGGGGGGGGGGG        000000000           333333333333333   \n  GGG::::::::::::G     00:::::::::00        3:::::::::::::::33 \n GG:::::::::::::::G   00:::::::::::::00     3::::::33333::::::3\nG:::::GGGGGGGG::::G  0:::::::000:::::::0    3333333     3:::::3\nG:::::G       GGGGGG 0::::::0   0::::::0                3:::::3\nG:::::G              0:::::0     0:::::0                3:::::3\nG:::::G              0:::::0     0:::::0        33333333:::::3 \nG:::::G    GGGGGGGGGG0:::::0 000 0:::::0        3:::::::::::3  \nG:::::G    G::::::::G0:::::0 000 0:::::0        33333333:::::3 \nG:::::G    GGGGG::::G0:::::0     0:::::0                3:::::3\nG:::::G        G::::G0:::::0     0:::::0                3:::::3\n G:::::G       G::::G0::::::0   0::::::0                3:::::3\n  G:::::GGGGGGGG::::G0:::::::000:::::::0    3333333     3:::::3\n   GG:::::::::::::::G 00:::::::::::::00     3::::::33333::::::3\n     GGG::::::GGG:::G  00:::::::::00        3:::::::::::::::33 \n        GGGGGG   GGGG    000000000           333333333333333   "
+
+
+sleep 1
+
+echo "\n---------------------"
+echo "| Starting minikube |"
+echo "---------------------\n"
+minikube start
+
+echo "\n---------------------------"
+echo "| Enabling metrics-server |"
+echo "---------------------------\n"
+minikube addons enable metrics-server
+
+sleep 1
+
+echo "\n--------------------------------------------"
+echo "| Enabling docker environment for minikube |"
+echo "--------------------------------------------\n"
+eval $(minikube -p minikube docker-env)
+echo "Docker environment enabled for minikube"
+
+sleep 1
+
+echo "\n---------------------------------"
+echo "| Building backend docker image |"
+echo "---------------------------------\n"
+docker build -t backend -f ../backend/Dockerfile.kubernetes ../backend
+
+echo "\n--------------------------------"
+echo "| Starting database deployment |"
+echo "--------------------------------\n"
+
+sleep 3
+minikube kubectl -- apply -f postgres-deployment.yaml
+minikube kubectl -- apply -f postgres-service.yaml
+minikube kubectl -- apply -f postgres-pvc.yaml
+
+echo "\n-------------------------------"
+echo "| Starting backend deployment |"
+echo "-------------------------------\n"
+
+sleep 3
+minikube kubectl -- apply -f app-deployment.yaml
+minikube kubectl -- apply -f app-service.yaml
+minikube kubectl -- apply -f app-config.yaml
+minikube kubectl -- apply -f app-pvc.yaml
+
+echo "\n---------------------"
+echo "| Calling dashboard |"
+echo "---------------------\n"
+minikube dashboard
+
+# echo "\n---------------------------------------------"
+# echo "| Port forwarding to access the application |"
+# echo "---------------------------------------------\n"
+# minikube service app-service --url &
