9bfa71e8edf96588e94ae51c10956a597de38dd5
Caio <99201276+cmtabr@users.noreply.github.com>
2024-05-24 06:23:58 -0300

    fix(video-record): :ambulance: Changes video recording script
    Write the right output to .mp4 file

--- src/ai_processor/main.py
+++ src/ai_processor/main.py
@@ -5,25 +5,26 @@ model = YOLO("config/prod.pt")
 cam = cv.VideoCapture(0)
 
 if not cam.isOpened():
-    print("Câmera não encontrada")
-    exit()
+   print("Câmera não encontrada")
+   exit()
 
 fourcc = cv.VideoWriter_fourcc(*'mp4v')
-out = cv.VideoWriter('output.mp4', fourcc, 30.0, (640, 400))
+out = cv.VideoWriter('output.mp4', fourcc, 60.0, (640, 480))
 
 while True:
    ret, frame = cam.read()
 
    result = model.predict(frame, conf=0.5) 
 
+   out.write(frame)
    if ret:
       frame_resultados = result[0].plot()
       cv.imshow("Resultados", frame_resultados)
 
       out.write(frame_resultados)
 
-      if cv.waitKey(1) == ord('q'):
-         break
+   if cv.waitKey(1) == ord('q'):
+      break
 
 cam.release()
 out.release()
