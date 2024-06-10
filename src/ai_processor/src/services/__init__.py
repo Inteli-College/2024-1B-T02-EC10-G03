a5d8c5e05dd1567bab2efc1a991e7691f74c3b3e
cmtabr <caio.abreu@sou.inteli.edu.br>
2024-06-10 10:35:42 -0300

    refactor(camera-signal-handler): :recycle: Refact code for improved control over recording

---
+++ src/ai_processor/src/services/__init__.py
@@ -0,0 +1,2 @@
+from .bucket_service import Bucket
+from .record_service import Record, Signal
--- src/ai_processor/src/services/bucket_service.py
+++ src/ai_processor/src/services/bucket_service.py
@@ -1,9 +1,8 @@
 import os
-from threading import Thread
 
 from boto3 import client
 
-class BucketService(Thread):
+class Bucket:
     def __init__(self):
         self.bucket_name = os.getenv('AWS_BUCKET_NAME')
         self.s3 = client('s3', 
@@ -12,10 +11,6 @@ class BucketService(Thread):
                         )
         self.file_path: str = None
 
-    @classmethod
-    def get_bucket(self):
-        return self.s3.get_bucket(Bucket=self.bucket_name)
-    
     def get_file_path(self, file_name):
         self.file_path = os.path.join(os.path.dirname(__file__), file_name)
     
